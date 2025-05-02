304b235f9c3519f6a8daec9c230e1707b38e3afe
alvarosz <alvarosz86@gmail.com>
2025-05-02 16:33:37 +0200

    Subo cambios del proyecto

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
 import { defineConfig } from "vite";
+import path from 'path';
 
 export default defineConfig({
-  base: "./", // Usa rutas relativas en los enlaces generados en el HTML
+  base: "/Ayuntamiento/", // Ruta base para GitHub Pages
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        pistas: path.resolve(__dirname, 'pistas.html'),
+        equipogobierno: path.resolve(__dirname, 'equipogobierno.html'),
+        servicios: path.resolve(__dirname, 'servicios.html'),
+        // Agrega más archivos si es necesario
+      },
+    },
+  },
 });
